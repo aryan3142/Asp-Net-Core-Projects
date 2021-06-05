5ec39ccfaa9ad13a4f5ade412b75ea261c1175d9
Aryan Khandelwal <arykhandelwal52@gmail.com>
2021-06-06 00:24:43 +0530

    Added MVC to blank Asp.Net Core OutlierBookStore Web Application

--- OutLierBookStore/OutLierBookStore/Startup.cs
+++ OutLierBookStore/OutLierBookStore/Startup.cs
@@ -14,8 +14,13 @@ namespace OutLierBookStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
+        //too add all the dependencies that we use in our application we use this method
+        
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +31,12 @@ namespace OutLierBookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+           app.UseRouting();
+        
+           app.UseEndpoints(endpoints =>
+          {
+              endpoints.MapDefaultControllerRoute();
             });
         }
     }
