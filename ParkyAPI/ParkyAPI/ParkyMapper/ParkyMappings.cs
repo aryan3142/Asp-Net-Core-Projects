0d089f0eb72bbca82c36fece261c1e1cea8f9cee
Aryan Khandelwal <arykhandelwal52@gmail.com>
2021-06-10 22:09:21 +0530

    Created National Park Web API (ParkyAPI) with ASP.NET Core Web Api implementation.

---
+++ ParkyAPI/ParkyAPI/ParkyMapper/ParkyMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.ParkyMapper
+{
+    public class ParkyMappings : Profile
+    {
+        public ParkyMappings()
+        {
+            CreateMap<NationalPark, NationalParkDto>().ReverseMap();
+        }
+    }
+}
